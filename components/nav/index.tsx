7d8ce42f47158fe84049f678f335173e741549a1
RiyasaatAhmed <riyasaatahmed24@gmail.com>
2025-03-06 17:38:58 +0600

    animation added on mobile navbar

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -7,7 +7,7 @@ import { Menu, X } from "lucide-react";
 import { useScreenType } from "@/hooks/use-screen-type";
 import { smoothScrollToSection } from "@/utils/smooth-scroll-to-section";
 import { navbar } from "@/constants/navbar";
-import { motion, useScroll, useSpring } from "framer-motion";
+import { motion, useScroll, useSpring, AnimatePresence } from "framer-motion";
 
 export function Nav() {
   const [scrolled, setScrolled] = useState(false);
@@ -24,13 +24,16 @@ export function Nav() {
   const [width, setWidth] = useState("0%");
 
   useEffect(() => {
-    return smoothProgress.onChange((latest) =>
+    return smoothProgress.on("change", (latest) =>
       setWidth(`${0 + latest * 100}%`)
     );
   }, [smoothProgress]);
 
   // Toggle mobile menu
-  const toggleMenu = useCallback(() => setIsOpen((prev) => !prev), []);
+  const toggleMenu = useCallback(() => {
+    setIsOpen((prev) => !prev);
+    setWidth("0%");
+  }, []);
 
   // Track scrolling to update navbar styles
   useEffect(() => {
@@ -39,12 +42,29 @@ export function Nav() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  // Prevent scrolling when menu is open
+  useEffect(() => {
+    if (isOpen && screenType === "mobile") {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, [isOpen, screenType]);
+
   return (
     <header className="fixed w-full top-0 left-0 z-50">
       <nav
         className={`transition-all duration-300 ${
           scrolled || (isOpen && screenType === "mobile")
-            ? "bg-white/70 dark:bg-black/70 backdrop-blur-lg py-4"
+            ? `${
+                isOpen
+                  ? "bg-white dark:bg-black"
+                  : "bg-white/70 dark:bg-black/70"
+              } backdrop-blur-lg py-6`
             : "py-6"
         }`}
       >
@@ -91,15 +111,15 @@ export function Nav() {
         </div>
 
         {/* Mobile Navigation */}
-        {isOpen && screenType === "mobile" ? (
-          <>
-            {/* Prevent scrolling when menu is open */}
-            <style jsx global>{`
-              body {
-                overflow: hidden;
-              }
-            `}</style>
-            <div className="absolute top-full left-0 w-full h-screen bg-white dark:bg-black shadow-md">
+        <AnimatePresence>
+          {isOpen && screenType === "mobile" ? (
+            <motion.div
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0, opacity: 0 }}
+              transition={{ duration: 1, type: "spring" }}
+              className="absolute top-full left-0 w-full h-screen bg-white dark:bg-black shadow-md origin-top-right"
+            >
               <div className="flex flex-col items-start space-y-4 p-4">
                 {navbar.map((item) => (
                   <Button
@@ -108,7 +128,9 @@ export function Nav() {
                     className="text-muted-foreground"
                     onClick={() => {
                       setIsOpen(false);
-                      smoothScrollToSection(item.toLowerCase(), { offset: 64 });
+                      smoothScrollToSection(item.toLowerCase(), {
+                        offset: 64,
+                      });
                     }}
                   >
                     {item}
@@ -116,9 +138,9 @@ export function Nav() {
                 ))}
                 <ThemeButton />
               </div>
-            </div>
-          </>
-        ) : null}
+            </motion.div>
+          ) : null}
+        </AnimatePresence>
       </nav>
 
       {/* Expanding Scroll Indicator */}
