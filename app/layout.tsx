8ada8592ef35e98d10eaca281c7dfc371140add6
RiyasaatAhmed <riyasaatahmed24@gmail.com>
2026-01-31 04:30:18 +0600

    feat: integrate Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -100,6 +101,7 @@ export default function RootLayout({
           <Nav />
           {children}
           <Footer />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
